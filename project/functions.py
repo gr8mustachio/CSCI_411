8c082d3c2d2f7a5d15b75679ebb43e6e5c3a062a
gr8mustachio <ltsmith@mail.csuchico.edu>
2023-03-23 12:18:51 -0700

    working on project

---
+++ project/functions.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import math
+def euclidian(source, dest):
+    """The Euclidian Distance Formula is used to find the distance between two points
+    This function takes in two coords with format [x,y] and returns the Euclidian distance between the two points
+
+    Returns:
+        _type_: float
+    """
+    distance = math.sqrt((dest[0] - source[0])**2 + (dest[1] - source[1])**2)
+    return distance
+
+def manhattan(source, dest):
+    """Manhattan Distabce Fr=ormula is isued to calculate the manhattan distance from two point
+
+    Args:
+        source (list): [x1, y1]
+        dest (list): [x2, y2]]
+
+    Returns:
+        _type_: int
+    """
+    distance = abs(source[0] - dest[0]) + abs(source[1] - dest[1])
+    return distance
--- project/main.py
+++ project/main.py
@@ -1,10 +1,40 @@
-import networkx as nx 
+import networkx as nx
+import math
+import matplotlib.pyplot as plt
+from functions import euclidian, manhattan
+
 print('hello')
 #what I want to do
 """
 Heuristics:
     Euclidian Distance
+    Manhattan Distance
     Lp Space Heuristics
     make sure to check office hour video to see other types of heuistics
 Classes:
 Make graphs"""
+g = nx.Graph()
+# g.add_node(2)
+# g.add_node(5)
+# g.add_edge(2, 5)
+g.add_edge(1,'G')
+# g.add_edge(2,3)
+# g.add_edge(3,4)
+# g.add_edge(1,4)
+# g.add_edge(1,5)
+g.add_edge(4,1)
+
+plt.figure(1)
+nx.draw(g, with_labels=True)
+plt.savefig("g.png")
+plt.close()
+
+plt.clf()
+
+# f = nx.Graph()
+# f = nx.grid_2d_graph(1,2)
+
+
+# f.add_edge(2,3)
+# nx.draw(f, with_labels=True)
+# plt.savefig("f.png")
